a3354a73a6db4a41d4915193bb8620cbb46957de
Mauricio Santiago <mauricio_santiago_20@hotmail.com>
2020-10-16 00:13:22 -0300

    Aula 04 - Criação do servidor, inicio database

--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
 // importar dependências
 const express = require('express');
+const path = require('path')
+const pages = require('./pages');
+
+// iniciando o express
 const server = express();
 
+server
+// utilizando os arquivos estáticos
+.use(express.static('public'))
+
+// configurar template engine
+.set('views', path.join(__dirname, "views"))
+.set('view engine', 'hbs')
+
 // criar uma rota
-server.get('/', () => {
-    console.log('Hello, World!')
-})
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
 
 // ligar o servidor
 server.listen(5500)
